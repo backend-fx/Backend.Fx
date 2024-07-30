47e2c3d201205b8ff5f788d8f4fe4d4afe5d9999
marc <marc@wittke-web.de>
2024-07-30 20:01:53 -0300

    formatting

--- src/Backend.Fx.Tests/Exceptions/TheNotFoundException.cs
+++ src/Backend.Fx.Tests/Exceptions/TheNotFoundException.cs
@@ -2,31 +2,30 @@
 using JetBrains.Annotations;
 using Xunit;
 
-namespace Backend.Fx.Tests.Exceptions
+namespace Backend.Fx.Tests.Exceptions;
+
+public class TheNotFoundException
 {
-    public class TheNotFoundException
+    [Fact]
+    public void CanBeThrownWithoutAnyParameters()
     {
-        [Fact]
-        public void CanBeThrownWithoutAnyParameters()
-        {
-            var exception = new NotFoundException();
-            Assert.Null(exception.EntityName);
-        }
+        var exception = new NotFoundException();
+        Assert.Null(exception.EntityName);
+    }
         
-        [Fact]
-        public void FillsNameAndIdProperties()
-        {
-            var exception = new NotFoundException<SomeEntity>(4711);
-            Assert.Equal("SomeEntity", exception.EntityName);
-            Assert.Equal(4711, exception.Id);
-        }
+    [Fact]
+    public void FillsNameAndIdProperties()
+    {
+        var exception = new NotFoundException<SomeEntity>(4711);
+        Assert.Equal("SomeEntity", exception.EntityName);
+        Assert.Equal(4711, exception.Id);
+    }
 
 
+    [UsedImplicitly]
+    private class SomeEntity(int id)
+    {
         [UsedImplicitly]
-        private class SomeEntity(int id)
-        {
-            [UsedImplicitly]
-            public int Id { get; } = id;
-        }
+        public int Id { get; } = id;
     }
 }
--- src/Backend.Fx.Tests/Exceptions/TheUnprocessableExceptionBuilder.cs
+++ src/Backend.Fx.Tests/Exceptions/TheUnprocessableExceptionBuilder.cs
@@ -2,71 +2,70 @@
 using JetBrains.Annotations;
 using Xunit;
 
-namespace Backend.Fx.Tests.Exceptions
+namespace Backend.Fx.Tests.Exceptions;
+
+public class TheUnprocessableExceptionBuilder
 {
-    public class TheUnprocessableExceptionBuilder
+    [Fact]
+    public void AddsExceptionWhenAggregateIsNull()
     {
-        [Fact]
-        public void AddsExceptionWhenAggregateIsNull()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.AddIfNull(null!, "is null");
-            Assert.Throws<UnprocessableException>(() => sut.Dispose());
-        }
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.AddIfNull(null!, "is null");
+        Assert.Throws<UnprocessableException>(() => sut.Dispose());
+    }
 
-        [Fact]
-        public void AddsNoExceptionWhenAggregateIsNotNull()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.AddIfNull(new object(), "is null");
-            sut.Dispose();
-        }
+    [Fact]
+    public void AddsNoExceptionWhenAggregateIsNotNull()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.AddIfNull(new object(), "is null");
+        sut.Dispose();
+    }
 
-        [Fact]
-        public void DoesNotThrowExceptionWhenNotAddingConditionalError()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.AddIf(false, "something is broken");
-            sut.Dispose();
-        }
+    [Fact]
+    public void DoesNotThrowExceptionWhenNotAddingConditionalError()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.AddIf(false, "something is broken");
+        sut.Dispose();
+    }
 
-        [Fact]
-        public void ThrowsExceptionWhenAddingConditionalError()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.AddIf(true, "something is broken");
-            Assert.Throws<UnprocessableException>(() => sut.Dispose());
-        }
+    [Fact]
+    public void ThrowsExceptionWhenAddingConditionalError()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.AddIf(true, "something is broken");
+        Assert.Throws<UnprocessableException>(() => sut.Dispose());
+    }
         
-        [Fact]
-        public void ThrowsExceptionWhenAddingConditionalKeyedError()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.AddIf("the key", true, "something is broken");
-            Assert.Throws<UnprocessableException>(() => sut.Dispose());
-        }
+    [Fact]
+    public void ThrowsExceptionWhenAddingConditionalKeyedError()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.AddIf("the key", true, "something is broken");
+        Assert.Throws<UnprocessableException>(() => sut.Dispose());
+    }
 
-        [Fact]
-        public void ThrowsExceptionWhenAddingError()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.Add("something is broken");
-            Assert.Throws<UnprocessableException>(() => sut.Dispose());
-        }
+    [Fact]
+    public void ThrowsExceptionWhenAddingError()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.Add("something is broken");
+        Assert.Throws<UnprocessableException>(() => sut.Dispose());
+    }
         
-        [Fact]
-        public void ThrowsExceptionWhenAddingKeyedError()
-        {
-            IExceptionBuilder sut = UnprocessableException.UseBuilder();
-            sut.Add("theKey", "something is broken");
-            Assert.Throws<UnprocessableException>(() => sut.Dispose());
-        }
+    [Fact]
+    public void ThrowsExceptionWhenAddingKeyedError()
+    {
+        IExceptionBuilder sut = UnprocessableException.UseBuilder();
+        sut.Add("theKey", "something is broken");
+        Assert.Throws<UnprocessableException>(() => sut.Dispose());
+    }
 
+    [UsedImplicitly]
+    private class SomeEntity
+    {
         [UsedImplicitly]
-        private class SomeEntity
-        {
-            [UsedImplicitly]
-            public int Id { get; }
-        }
+        public int Id { get; }
     }
 }
